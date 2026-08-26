5e476f51a00042a89180041c11fba75b6e6f7bbe
Abhay <bklucky1976@gmail.com>
2026-08-26 20:53:45 +0530

    last change

--- Backend/server.js
+++ Backend/server.js
@@ -10,6 +10,8 @@ initSocket(httpServer);
 
 connectToDb();
 
-httpServer.listen(3000, () => {
-    console.log("Server is running on port 3000");
+const PORT = process.env.PORT || 3000;
+
+httpServer.listen(PORT, "0.0.0.0", () => {
+    console.log(`Server is running on port ${PORT}`);
 });
